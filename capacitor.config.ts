a4e066906be68ab3f30985041d5c308b4fbceab9
Bilgin Esme <besme@esme.org>
2026-04-15 19:28:35 +0300

    capacitor.config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,18 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.bilginesme.velop',
   appName: 'the-velop',
-  webDir: 'www'
+  webDir: 'www',
+  // Add this section below
+  plugins: {
+    CapacitorSQLite: {
+      iosIsEncryption: false,
+      iosKeychainPrefix: 'the-velop-sqlite',
+      iosBiometric: {
+        biometricAuth: false,
+        biometricTitle : "Biometric login for query"
+      }
+    }
+  }
 };
 
 export default config;
